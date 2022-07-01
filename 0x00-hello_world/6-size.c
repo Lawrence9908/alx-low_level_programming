0831dcbf7aee2b93842b12eb4f3e486e8aeef38d
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-06-30 18:41:21 -0700

    C progarmming Hello, World first commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main - main functions
+ *main - main function
  *
  *
  *Return: return 0
@@ -12,6 +12,5 @@ int main(void)
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %d bytes(s)\n", sizeof(float));
-
 	return (0);
 }
