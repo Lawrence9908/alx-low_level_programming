cba6e1b94a266c8e71c35e79fdbf747d81007ba3
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-01 14:13:14 -0700

    C Programming condition statements

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -13,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if(n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("is positive\n");
 	}
 	if else(n == 0)
 	{
-		printf("%d is zero\n",n);
+		printf("is zero\n");
 	}
 	else
 	{
-		printf("%d is negative\n",n);
+		printf("is negative\n");
 	}
 	return (0);
 }
