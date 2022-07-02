85ca479830378cec048c57a652aed06a7a9c31d0
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-01 17:36:03 -0700

    C Programming condition statements

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 
 	if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n , last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
@@ -25,7 +25,8 @@ int main(void)
 		{
 			printf("Last digit of %d is %d  and greater than 5\n", n, last_digit);
 		}
-		else{
+		else if (last_digit < 6)
+		{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 		}
 	}
