983130463bffed749eb5fc596619e5e269d73345
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-01 19:23:08 -0700

    C Programming condition statements

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,16 +3,16 @@
  *main - entry point of the program
  *The program will print all different possible combination
  *of two digits
- *Return: return 0 
+ *Return: return 0
  */
 int main(void)
 {
 	int num1, num2;
-	
+
 	for (num1 = 0; num1 < 9; num1++)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
-	        {
+		{
 			putchar((num1 % 10) + '0');
 			putchar((num2 % 10) + '0');
 
