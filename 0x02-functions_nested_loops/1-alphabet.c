01a6ab3d4c5d60a63764f72e96447baf1af0a482
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-06 13:18:47 -0700

    functions and nested loops programs

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
 
 void print_alphabet(void)
 {
-	char alphabet;
+	int alphabet;
 	
 	for (alphabet = 'a'; alphabet <='z'; alphabet++)
 	{
 		_putchar(alphabet);
 	}
-	_putchar('\n');
+	_putchar(10);
 }
