c6498ce49e128a7687be01f96532768d1c5661ac
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-06 15:41:05 -0700

    functions and nested loops programs

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,17 +15,25 @@ void times_table(void)
 		{
 			multi = x * y;
 			if (x == 0)
+			{
 				_putchar(multi + '0');
+			}
 			else if (multi >= 10)
+			{
 				_putchar('\n');
 				_putchar(multi / 10 + '0');
 				_putchar(multi % 10 + '0');
+			}
 			else
+			{
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(multi + '0');
+			}
 			if (x != 9)
+			{
 				_putchar(',');
+			}
 		}
 		_putchar('\n');
 	}
