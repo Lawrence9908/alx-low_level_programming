3764914d4644b4dcd9c27b2ca212c69ede73aea7
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-06 15:45:42 -0700

    functions and nested loops programs

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  *print_last_digit -  prints the last digit of input
- *
+ *@c: input value
  *Return: always 0
  */
 int print_last_digit(int c)
@@ -9,7 +9,9 @@ int print_last_digit(int c)
 	c = c % 10;
 
 	if (c < 0)
-		c = - c;
+	{
+		c = -c;
 		_putchar(c + '0');
 		return (c);
+	}
 }
