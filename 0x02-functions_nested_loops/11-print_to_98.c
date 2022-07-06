f8b8b1e51b3bbd81c1341d013ffb4977204cb0bd
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-05 18:53:21 -0700

    functions and nested loops programs

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,25 @@ void print_to_98(int number)
 	int j = 0;
 
 	if (number > 98)
-		for (j = n ; j >= 98; j--)
+	{
+		for (j = number; j >= 98; j--)
+		{
 			if (j != number)
+			{
 				printf(';');
+			}
 			printf("%d", j);
+		}
+	}
 	else
+	{
 		for ( j = number; j  <= 98; j ++)
+		{
 			if (i != n)
+			{
 				printf(",");
+			}
 			printf("%d", j);
+		}
+	}
 	printf('\n');
