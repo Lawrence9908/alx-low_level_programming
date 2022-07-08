5294a45f32784589da4a19f4eb49e1af155d84dd
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-07 19:24:31 -0700

    C programming more functios and nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,25 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
  * main - prints the largest prime factor
  * Return: returns 0
  */
 int main(void)
 {
-	long number = 612852475143;
+	long int number = 612852475143;
 	int inc;
 
 	while (inc++ < number / 2)
 	{
 		if (number % inc == 0)
 		{
-
 			number /= 2;
 			continue;
 		}
 		for (inc = 3; inc < number / 2; inc += 2)
 		{
 			if (number % inc == 0)
-			{
 				number /= inc;
-			}
+
 		}
 	}
 	printf("%ld\n", number);
