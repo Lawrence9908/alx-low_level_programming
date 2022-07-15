fa737b57db699b37c2607d20b50f5388cd822e07
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-15 04:18:51 -0700

    C Programming Pointers, arrays and Strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,7 +5,7 @@
  * @src: string to be concatinated to dest string
  * Return: returns a pointer to the resulting string dest
  */
-char *_strcat(char *dest, char *src
+char *_strcat(char *dest, char *src)
 {
 	int i = 0, dest_length = 0;
 
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,45 +1,23 @@
 #include "main.h"
-
 /**
- *
- *  *_strncat - concatenate two strings but add inputted number of bytes
- *
- *   *@dest: string to be appended upon
- *
- *    *@src: string to be completed at end of dest
- *
- *     *@n:integer parameter to compare index to
- *
- *      *Return: returns new concatenated string
- *
- *       */
-
-
-
+ * _strncat - concatenate two strings but add inputted number of bytes
+ * @dest: the string to be appended upon
+ * @src: the string to be completed at end of dest
+ * @n: input
+ * Return: returns a a pointer to the resulting string dest
+ */
 char *_strncat(char *dest, char *src, int n)
-
 {
+	int i = 0, dest_length = 0;
 
+	while (dest[i++])
+	{
+		dest_length++;
+	}
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[dest_length++] = src[i];
+	}
 
-
-		int index = 0, dest_len = 0;
-
-
-
-			while (dest[index++])
-
-						dest_len++;
-
-
-
-				for (index = 0; src[index] && index < n; index++)
-
-							dest[dest_len++] = src[index];
-
-
-
-					return (dest);
-
-
-
+	return (dest);
 }
