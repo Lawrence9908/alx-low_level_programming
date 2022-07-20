013b8ea4484bddfb910d50b0b8ea51784a94a090
Lawrence Mugwena <mugwenalawrence121@gmail.com>
2022-07-19 18:17:36 -0700

    C programming Even more pointers, arrays and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 #include <stdio.h>
 /**
  *char prototype
@@ -12,3 +15,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+#endif /* MAIN_H */
